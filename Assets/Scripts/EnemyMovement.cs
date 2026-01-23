503882e882b58442be5a72c37b551a4606674363
Tamamocross2004 <tamamocross@sjtu.edu.cn>
2026-01-23 18:17:44 +0800

    实现敌人攻击动画

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed;
+    public float attackRange = 2;
+    public float attackCooldown = 2;
+    public float playerDetectRange = 5;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
+
+    private float attackCooldownTimer;
     private int facingDirection = -1; // 表示初始面向右边
     private EnemyState enemystate;
+
 
     private Rigidbody2D rb;
     private Transform player;
@@ -23,18 +31,32 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(enemystate == EnemyState.Chasing)
+        CheckForPlayer();
+        if(attackCooldownTimer > 0)
         {
-            if((player.position.x > transform.position.x && facingDirection == -1) ||
-                (player.position.x < transform.position.x && facingDirection == 1))
-            {
-                Flip();
-            }
-
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.velocity = direction * speed;
+            attackCooldownTimer -= Time.deltaTime;
         }
 
+        if(enemystate == EnemyState.Chasing)
+        {
+            Chase();
+        }
+        else if(enemystate == EnemyState.Attacking)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void Chase()
+    {
+        if((player.position.x > transform.position.x && facingDirection == -1) ||
+            (player.position.x < transform.position.x && facingDirection == 1))
+        {
+            Flip();
+        }
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity = direction * speed;
     }
 
     void Flip()
@@ -43,26 +65,34 @@ public class EnemyMovement : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void CheckForPlayer()
     {
-        if(collision.gameObject.tag == "Player")
-        {
-            if(player == null)
-            {
-                player = collision.transform;
-            }
-            ChangeState(EnemyState.Chasing);
-        }
-    }
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
+        if(hits.Length > 0)
+        {
+            player = hits[0].transform;
+
+            // 检查玩家是否在攻击范围内, 以及冷却时间是否就绪
+            if(Vector2.Distance(transform.position, player.position) <= attackRange && attackCooldownTimer <= 0)
+            {
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
+            }
+
+            // 若距离大于攻击范围，进入追逐状态
+            else if(Vector2.Distance(transform.position, player.position) > attackRange)
+            {
+                ChangeState(EnemyState.Chasing);
+            }
+        }
+        else
         {
             rb.velocity = Vector2.zero;
             ChangeState(EnemyState.Idle);
         }
     }
+
 
     void ChangeState(EnemyState newState)
     {
@@ -74,6 +104,10 @@ public class EnemyMovement : MonoBehaviour
         else if (enemystate == EnemyState.Chasing)
         {
             anim.SetBool("isChasing", false);
+        }
+        else if (enemystate == EnemyState.Attacking)
+        {
+            anim.SetBool("isAttacking", false);
         }
 
         //更新当前状态 
@@ -88,6 +122,16 @@ public class EnemyMovement : MonoBehaviour
         {
             anim.SetBool("isChasing", true);
         }
+        else if (enemystate == EnemyState.Attacking)
+        {
+            anim.SetBool("isAttacking", true);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
     }
 }
 
@@ -95,4 +139,5 @@ public enum EnemyState
 {
     Idle,
     Chasing,
+    Attacking
 }
