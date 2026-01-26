a1cfbb032ed980429d00db5352526ce688e73450
Tamamocross2004 <tamamocross@sjtu.edu.cn>
2026-01-26 22:42:31 +0800

    增加经验系统，使用观察者模式进行脚本通信

---
+++ Assets/Scripts/PlayerScripts/ExpManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ExpManager : MonoBehaviour
+{
+    public int level;
+    public int currentExp;
+    public int expToLevel = 10;
+    public float expGrowthMultiplier = 1.2f;
+    public Slider expSlider;
+    public TMP_Text currentLevelText;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            GainExperience(2);
+        }
+    }
+
+    private void OnEnable()
+    {
+        EnemyHealth.OnMonsterDefeated += GainExperience;
+    }
+
+    private void OnDisable()
+    {
+        EnemyHealth.OnMonsterDefeated -= GainExperience;
+    }
+
+    public void GainExperience(int amount)
+    {
+        currentExp += amount;
+        if(currentExp >= expToLevel)
+        {
+            LevelUp();
+        }
+
+        UpdateUI();
+    }
+
+    private void LevelUp()
+    {
+        level++;
+        currentExp -= expToLevel;
+        expToLevel = Mathf.RoundToInt(expToLevel * expGrowthMultiplier);
+    }
+
+    public void UpdateUI()
+    {
+        expSlider.maxValue = expToLevel;
+        expSlider.value = currentExp;
+        currentLevelText.text = "Level: " + level;
+    }
+}
