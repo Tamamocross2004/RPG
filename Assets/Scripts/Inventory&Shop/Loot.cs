fdb031524f704ea0391eae5934663fa1b8f443c7
Tamamocross2004 <tamamocross@sjtu.edu.cn>
2026-02-03 22:59:22 +0800

    实现物品进入UI

--- Assets/Scripts/Inventory&Shop/Loot.cs
+++ Assets/Scripts/Inventory&Shop/Loot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Loot : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Loot : MonoBehaviour
     public Animator anim;
 
     public int quantity;
+    public static event Action<ItemSO, int> OnItemLooted;
 
     private void OnValidate()
     {
@@ -25,6 +27,7 @@ public class Loot : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             anim.Play("LootPickup");
+            OnItemLooted?.Invoke(itemSO, quantity);
             Destroy(gameObject, 0.5f);
         }
     }
