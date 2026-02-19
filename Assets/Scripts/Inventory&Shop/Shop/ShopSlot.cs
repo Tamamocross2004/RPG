0ec65c3afec2863a65976d3f70fdd23904d8f88f
Tamamocross2004 <tamamocross@sjtu.edu.cn>
2026-02-19 21:24:38 +0800

    实现商店物品的填充

---
+++ Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Image itemImage;
+
+    private int price;
+
+    public void Initialize(ItemSO newItemSO, int price)
+    {
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.itemName;
+        this.price = price;
+        priceText.text = price.ToString();
+    }
+}
