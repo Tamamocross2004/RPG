428db0f60b46b0729deb1477115325cf4a8445dc
Tamamocross2004 <tamamocross@sjtu.edu.cn>
2026-02-24 22:40:13 +0800

    实现场景切换

---
+++ Assets/Scripts/SceneChange/SceneChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            fadeAnim.Play("FadeToWhite");
+            StartCoroutine(DelayFade());
+        }
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
